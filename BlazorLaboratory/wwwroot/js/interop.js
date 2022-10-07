8e37a5e271f36f4550e38148043e989abab164ad
jcielecki_ <jacekc1999@o2.pl>
2022-10-07 16:08:55 +0200

    add svg map

---
+++ BlazorLaboratory/wwwroot/js/interop.js
@@ -0,0 +1,27 @@
+﻿function addEvents() {
+    var tooltip = document.querySelector('.map-tooltip');
+
+    // iterate throw all `path` tags
+    [].forEach.call(document.querySelectorAll('path.HI-map'), function (item) {
+        // attach click event, you can read the URL from a attribute for example.
+        item.addEventListener('click', function () {
+            window.open('http://google.co.il')
+        });
+
+        // attach mouseenter event
+        item.addEventListener('mouseenter', function () {
+            var sel = this,
+                // get the borders of the path - see this question: http://stackoverflow.com/q/10643426/863110
+                pos = sel.getBoundingClientRect()
+
+            tooltip.style.display = 'block';
+            tooltip.style.top = pos.top + 'px';
+            tooltip.style.left = pos.left + 'px';
+        });
+
+        // when mouse leave hide the tooltip
+        item.addEventListener('mouseleave', function () {
+            tooltip.style.display = 'none';
+        });
+    });
+}
