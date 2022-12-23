07042280092e05b2b3e32ae2da2defc11e79fb99
Jacek Cielecki <jcielecki@sii.pl>
2022-12-23 13:31:48 +0100

    Add api with user crud

---
+++ BlazorLaboratory.WebApi/Api.cs
@@ -0,0 +1,82 @@
+﻿using BlazorLaboratory.DataAccess.Data;
+
+namespace BlazorLaboratory.WebApi;
+
+public static class Api
+{
+    public static void ConfigureApi(this WebApplication app)
+    {
+        //All of endpoint mapping 
+        app.MapGet("/Users", GetUsers);
+        app.MapGet("/Users/{id}", GetUser);
+        app.MapPost("/Users", InsertUser);
+        app.MapPut("/Users", UpdateUser);
+        app.MapDelete("/Users", DeleteUser);
+    }
+
+    private static async Task<IResult> GetUsers(IUserData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetUsers());
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
+    }
+
+    private static async Task<IResult> GetUser(int id, IUserData data)
+    {
+        try
+        {
+            var results = await data.GetUser(id);
+            return results is null ? Results.NotFound() : Results.Ok(results);
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
+    }
+
+    private static async Task<IResult> InsertUser(UserModel user, IUserData data)
+    {
+        try
+        {
+            await data.InsertUser(user);
+            return Results.Ok();
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
+    }
+
+    private static async Task<IResult> UpdateUser(UserModel user, IUserData data)
+    {
+        try
+        {
+            await data.UpdateUser(user);
+            return Results.Ok();
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
+    }
+
+    private static async Task<IResult> DeleteUser(int id, IUserData data)
+    {
+        try
+        {
+            await data.DeleteUser(id);
+            return Results.Ok();
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
+    }
+
+
+}
--- BlazorLaboratory.WebApi/Program.cs
+++ BlazorLaboratory.WebApi/Program.cs
@@ -1,13 +1,15 @@
+using BlazorLaboratory.DataAccess.Data;
+using BlazorLaboratory.WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+builder.Services.AddSingleton<IUserData, UserData>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -15,29 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.ConfigureApi();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
