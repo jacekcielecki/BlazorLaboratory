162fcb188ab8c416d3e6ce1a6135475275d6fa81
Jacek Cielecki <jcielecki@sii.pl>
2023-01-04 17:00:57 +0100

    Add SignalR Tests

--- BlazorLaboratory.BlazorUI/Program.cs
+++ BlazorLaboratory.BlazorUI/Program.cs
@@ -6,13 +6,12 @@ using MudBlazor.Services;
 using Refit;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var appConfig = builder.Configuration;
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-builder.Services.AddRefitClient<IUserClient>().ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7234"));
-
-
+builder.Services.AddRefitClient<IUserClient>().ConfigureHttpClient(c => c.BaseAddress = new Uri(appConfig["ApiUri"]!));
 
 await builder.Build().RunAsync();
