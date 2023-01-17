3627747ea44552c41377a7438eb03fa3c23eee88
Jacek Cielecki <jcielecki@sii.pl>
2023-01-17 17:09:33 +0100

    add counter confirmation to blazor server

---
+++ BlazorLaboratory.BlazorServer/Helpers/CounterHubHelper.cs
@@ -0,0 +1,21 @@
+﻿using BlazorLaboratory.BlazorServer.Helpers;
+using BlazorLaboratory.BlazorServer.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorLaboratory.BlazorServer.Helpers;
+
+public class CounterHubHelper : ICounterHubHelper
+{
+    private readonly IHubContext<CounterHub> _hubContext;
+
+    public CounterHubHelper(IHubContext<CounterHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+
+    public void ConfirmIncrementCounter()
+    {
+        _hubContext.Clients.All.SendAsync("CounterIncrementConfirmation");
+    }
+}
---
+++ BlazorLaboratory.BlazorServer/Hubs/CounterHub.cs
@@ -0,0 +1,16 @@
+﻿using BlazorLaboratory.BlazorServer.Helpers;
+using Hangfire;
+using Microsoft.AspNetCore.SignalR;
+
+
+namespace BlazorLaboratory.BlazorServer.Hubs;
+
+public class CounterHub : Hub
+{
+    public async Task AddToTotal(string user, int value)
+    {
+        await Clients.All.SendAsync("CounterIncrement", user, value);
+        BackgroundJob.Schedule<CounterHubHelper>(h => h.ConfirmIncrementCounter(), TimeSpan.FromSeconds(3));
+    }
+
+}
