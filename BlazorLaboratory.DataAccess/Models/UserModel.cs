ce8eaa649454491c0751b0f5bb1dc5e4bb1bb287
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-02-27 20:28:23 +0100

    add contact details model

--- BlazorLaboratory.DataAccess/Models/UserModel.cs
+++ BlazorLaboratory.DataAccess/Models/UserModel.cs
@@ -5,4 +5,5 @@ public class UserModel
     public int Id { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+    public int ContactDetailsId { get; set; }
 }
