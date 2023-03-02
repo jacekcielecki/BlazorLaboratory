7c565371ff744f3f73a71a1e4c35d0c491d4d292
Jacek Cielecki <jcielecki@sii.pl>
2023-03-02 16:33:07 +0100

    add ExpansionTable

---
+++ BlazorLaboratory.BlazorUI/Dto/UserAccessDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorLaboratory.BlazorUI.Dto;
+
+public class UserAccessDto
+{
+    public string Email { get; set; }
+    public string Division { get; set; }
+    public string Country { get; set; }
+    public string Level { get; set; }
+    public string Role { get; set; }
+}
