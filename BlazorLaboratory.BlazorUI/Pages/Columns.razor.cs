d5bee786552e993ff997cbd96d3420f2b893c0a0
Jacek Cielecki <jcielecki@sii.pl>
2023-07-06 11:49:54 +0200

    Update Columns.razor

---
+++ BlazorLaboratory.BlazorUI/Pages/Columns.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace BlazorLaboratory.BlazorUI.Pages;
+
+
+public partial class Columns
+{
+    private string _content;
+
+    protected override void OnInitialized()
+    {
+        string partnerHubUrl = "https://www.google.pl";
+        string html1 = $"You cannot unlock the project in Building Planner because it has been locked in the <a href=\"{partnerHubUrl}\" class=\"font-weight-bold link-dark\" target=\"_blank\">Partner Hub</a>. Go to the <a href=\"{partnerHubUrl}\" class=\"font-weight-bold link-dark\" target=\"_blank\">Partner Hub</a> to unlock the project.";
+       
+        _content = string.Format(html1, partnerHubUrl);
+
+        _snackbar.Add(_content, Severity.Error);
+    }
+}
