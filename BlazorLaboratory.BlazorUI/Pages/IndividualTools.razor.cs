bdffb640b60d1a05f9f6a54043242173118401ad
Jacek Cielecki <jcielecki@sii.pl>
2023-08-09 10:26:11 +0200

    Add individual tools

---
+++ BlazorLaboratory.BlazorUI/Pages/IndividualTools.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorLaboratory.BlazorUI.Pages;
+
+public partial class IndividualTools
+{
+    private string _description = "Blazor Laboratory solution which is a Smart Blazor testing playground for large buildings. This application facilitates the import and export of image files in JPG format for the creation of building structure automatically, building planning manually, selection of products (like sensors, lights, temperature control devices, blinds, sockets, etc.), and its assortments.";
+
+}
