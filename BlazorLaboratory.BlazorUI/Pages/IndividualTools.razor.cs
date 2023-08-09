9ff4b9aa2c4036b369164c63cbf5a532d35a0d6b
Jacek Cielecki <jcielecki@sii.pl>
2023-08-09 12:44:25 +0200

    BlazorUI add MechanicalPlaning

--- BlazorLaboratory.BlazorUI/Pages/IndividualTools.razor.cs
+++ BlazorLaboratory.BlazorUI/Pages/IndividualTools.razor.cs
@@ -2,6 +2,6 @@
 
 public partial class IndividualTools
 {
-    private string _description = "Blazor Laboratory solution which is a Smart Blazor testing playground for large buildings. This application facilitates the import and export of image files in JPG format for the creation of building structure automatically, building planning manually, selection of products (like sensors, lights, temperature control devices, blinds, sockets, etc.), and its assortments.";
-
+    private const string _description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    private const string _url = "https://www.google.pl/";
 }
