9ced742bc713fec68fcab5c6f8675f8c9a300501
Jacek Cielecki <jcielecki@sii.pl>
2023-08-14 16:50:38 +0200

    BlazorUI add configuration of security headers

---
+++ BlazorLaboratory.BlazorUI/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorLaboratory.BlazorUI;
+
+public static class Extensions
+{
+    public static void AddDefaultSecurityHeaders(this HttpClient client)
+    { 
+        client.DefaultRequestHeaders.Add("Content-Security-Policy", 
+            new List<string> { "default-src https: object-src 'none'", "default-src 'self'; script-src *.trusted.com"});
+        client.DefaultRequestHeaders.Add("X-XSS-Protection", "1; mode=block");
+        client.DefaultRequestHeaders.Add("X-Frame-Options", 
+            new List<string> { "DENY", "SAMEORIGIN" });
+        client.DefaultRequestHeaders.Add("X-Content-Type-Options", "nosniff");
+        client.DefaultRequestHeaders.Add("Strict-Transport-Security", "Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
+    }
+}
--- BlazorLaboratory.BlazorUI/Program.cs
+++ BlazorLaboratory.BlazorUI/Program.cs
@@ -12,6 +12,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-builder.Services.AddRefitClient<IUserClient>().ConfigureHttpClient(c => c.BaseAddress = new Uri(appConfig["ApiUri"]!));
+builder.Services.AddRefitClient<IUserClient>().ConfigureHttpClient(httpClient =>
+{
+    httpClient.BaseAddress = new Uri(appConfig["ApiUri"]!);
+    httpClient.AddDefaultSecurityHeaders();
+});
 
 await builder.Build().RunAsync();
