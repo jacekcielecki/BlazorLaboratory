e11d8dee17e12b1eab9aa116c1895e5b61b74149
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-08-14 18:19:46 +0200

    BlazorUI cleanup

---
+++ BlazorLaboratory.BlazorUI/Pages/GraphPage.razor.cs
@@ -0,0 +1,18 @@
+﻿using MudBlazor;
+
+namespace BlazorLaboratory.BlazorUI.Pages;
+
+public partial class GraphPage
+{
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            var valueFromGraph = await GraphClient.GetInstructions.ExecuteAsync();
+        }
+        catch (Exception e)
+        {
+            Snackbar.Add(e.Message, Severity.Info);
+        }
+    }
+}
--- BlazorLaboratory.BlazorUI/Program.cs
+++ BlazorLaboratory.BlazorUI/Program.cs
@@ -7,20 +7,17 @@ using Refit;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 var appConfig = builder.Configuration;
-var graphUri = "https://localhost:7258/graphql";
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddGraphClient().ConfigureHttpClient(client => client.BaseAddress = new Uri(appConfig["GraphUri"]!));
 builder.Services.AddRefitClient<IUserClient>().ConfigureHttpClient(httpClient =>
 {
     httpClient.BaseAddress = new Uri(appConfig["ApiUri"]!);
     httpClient.AddDefaultSecurityHeaders();
 });
-
-builder.Services.AddGraphClient()
-    .ConfigureHttpClient(client => client.BaseAddress = new Uri(graphUri));
 
 
 await builder.Build().RunAsync();
