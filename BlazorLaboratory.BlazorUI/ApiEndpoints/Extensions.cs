4f51096445d8b4213beed2eb01bc05118f131e26
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-09-12 18:49:55 +0200

    BlazorUI adjust Extensions namespace

--- BlazorLaboratory.BlazorUI/ApiEndpoints/Extensions.cs
+++ BlazorLaboratory.BlazorUI/ApiEndpoints/Extensions.cs
@@ -1,13 +1,13 @@
-﻿namespace BlazorLaboratory.BlazorUI;
+﻿namespace BlazorLaboratory.BlazorUI.ApiEndpoints;
 
 public static class Extensions
 {
     public static void AddDefaultSecurityHeaders(this HttpClient client)
-    { 
-        client.DefaultRequestHeaders.Add("Content-Security-Policy", 
-            new List<string> { "default-src https: object-src 'none'", "default-src 'self'; script-src *.trusted.com"});
+    {
+        client.DefaultRequestHeaders.Add("Content-Security-Policy",
+            new List<string> { "default-src https: object-src 'none'", "default-src 'self'; script-src *.trusted.com" });
         client.DefaultRequestHeaders.Add("X-XSS-Protection", "1; mode=block");
-        client.DefaultRequestHeaders.Add("X-Frame-Options", 
+        client.DefaultRequestHeaders.Add("X-Frame-Options",
             new List<string> { "DENY", "SAMEORIGIN" });
         client.DefaultRequestHeaders.Add("X-Content-Type-Options", "nosniff");
         client.DefaultRequestHeaders.Add("Strict-Transport-Security", "Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
