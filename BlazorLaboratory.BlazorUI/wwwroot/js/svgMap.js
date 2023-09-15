8976095566a77f664048a93ab25c769d9680fe6c
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-09-15 20:35:11 +0200

    BlazorUI fix SvgMap

--- BlazorLaboratory.BlazorUI/wwwroot/js/svgMap.js
+++ BlazorLaboratory.BlazorUI/wwwroot/js/svgMap.js
@@ -1,24 +1,28 @@
 ﻿window.svgMap = {
     init: function () {
-
         var tooltip = document.querySelector('.map-tooltip');
+        var counties = ["Niihau", "Kauai", "Oahu", "Molokai", "Lanai", "Kahoolawe", "Maui", "Hawaii"];
+        var contents = {};
+        for (var i = 1; i <= 8; i++) {
+            contents['tooltip' + i] = counties[i - 1];
+        }
 
-        // iterate through all `path` tags
+        // iterate throw all `path` tags
         [].forEach.call(document.querySelectorAll('path.HI-map'), function (item) {
             // attach click event, you can read the URL from a attribute for example.
             item.addEventListener('click', function () {
-                window.open('http://google.co.il')
+                window.open(this.getAttribute('data-link'));
             });
 
             // attach mouseenter event
             item.addEventListener('mouseenter', function () {
-                var sel = this,
-                    // get the borders of the path - see this question: http://stackoverflow.com/q/10643426/863110
-                    pos = sel.getBoundingClientRect()
-
+                tooltip.innerHTML = contents[item.getAttribute('data-tooltip')];
                 tooltip.style.display = 'block';
-                tooltip.style.top = pos.top + 'px';
-                tooltip.style.left = pos.left + 'px';
+            });
+
+            item.addEventListener('mousemove', function (e) {
+                tooltip.style.top = e.clientY + 'px';
+                tooltip.style.left = e.clientX + 'px';
             });
 
             // when mouse leave hide the tooltip
