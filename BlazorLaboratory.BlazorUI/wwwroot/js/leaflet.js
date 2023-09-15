8e4302397a43f77f69973c27783c25226be135e8
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-09-15 21:33:21 +0200

    BlazorUI leaflet add marker on button

--- BlazorLaboratory.BlazorUI/wwwroot/js/leaflet.js
+++ BlazorLaboratory.BlazorUI/wwwroot/js/leaflet.js
@@ -1,10 +1,17 @@
+var leafletMap; // map reference
+
 window.blazorExtensions = {
+
     initMap: function () {
-        var map = L.map('map').setView([51.505, -0.09], 13);
+        leafletMap = L.map('map').setView([51.505, -0.09], 13);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+        }).addTo(leafletMap);
+    },
+
+    addMarker: function (lon, lat) {
+        var marker = L.marker([lon, lat]).addTo(leafletMap);
     },
 };
