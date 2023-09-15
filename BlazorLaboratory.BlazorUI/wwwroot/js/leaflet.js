3853896df2077e38a6f8d122c32b290296619c1c
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-09-15 19:29:24 +0200

    BlazorUI add leaflet

---
+++ BlazorLaboratory.BlazorUI/wwwroot/js/leaflet.js
@@ -0,0 +1,10 @@
+window.blazorExtensions = {
+    initMap: function () {
+        var map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    },
+};
