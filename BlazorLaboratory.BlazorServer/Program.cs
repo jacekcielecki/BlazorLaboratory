765cd9b4223e2af72e4e09c56b5e2b34b97940f3
Jacek Cielecki <jcielecki@sii.pl>
2023-11-05 13:04:09 +0100

    BlazorServer add string localization

--- BlazorLaboratory.BlazorServer/Program.cs
+++ BlazorLaboratory.BlazorServer/Program.cs
@@ -11,6 +11,24 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddMudServices();
+builder.Services.AddControllers();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+RequestLocalizationOptions GetLocalizationOptions()
+{
+    var cultures = builder?.Configuration
+        .GetSection("Cultures")
+        .GetChildren()
+        .ToDictionary(x => x.Key, x => x.Value);
+
+    var supportedCultures = cultures?.Keys.ToArray();
+
+    var localizationOptions = new RequestLocalizationOptions()
+        .AddSupportedCultures(supportedCultures!)
+        .AddSupportedUICultures();
+
+    return localizationOptions;
+}
 
 builder.Services.AddResponseCompression(options =>
 {
@@ -47,12 +65,13 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
+app.UseRequestLocalization(GetLocalizationOptions());
 
 app.UseRouting();
 app.UseCors("CorsPolicy");
 
+app.MapControllers();
 app.MapBlazorHub();
 app.MapHub<ChatHub>("/chathub");
 app.MapHub<CounterHub>("/counterhub");
