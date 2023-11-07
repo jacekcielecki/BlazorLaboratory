8715cf097562be473e5e757b401c4e65e09c3af3
Jacek Cielecki <jcielecki@sii.pl>
2023-11-07 11:07:07 +0100

    BlazorUI Lorem ipsum

--- BlazorLaboratory.BlazorUI/Pages/CSS/Columns.razor.cs
+++ BlazorLaboratory.BlazorUI/Pages/CSS/Columns.razor.cs
@@ -8,9 +8,11 @@ public partial class Columns
 
     protected override void OnInitialized()
     {
-        string partnerHubUrl = "https://www.google.pl";
-        string message = $"You cannot unlock the project in Building Planner because it has been locked in the <a href=\"{partnerHubUrl}\" class=\"font-weight-bold link-dark\" target=\"_blank\">Partner Hub</a>. Go to the <a href=\"{partnerHubUrl}\" class=\"font-weight-bold link-dark\" target=\"_blank\">Partner Hub</a> to unlock the project.";
-        _content = string.Format(message, partnerHubUrl);
+        string googleUrl = "https://www.google.pl";
+        string message = $"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore " +
+                         $"<a href=\"{googleUrl}\" class=\"font-weight-bold link-dark\" target=\"_blank\">et dolore magna aliqua.</a>. " +
+                         $"Ut enim ad minim veniam <a href=\"{googleUrl}\" class=\"font-weight-bold link-dark\" target=\"_blank\">Google</a> , quis nostrud exercitation ullamco laboris nisi.";
+        _content = string.Format(message, googleUrl);
         Snackbar.Add(_content, Severity.Error);
     }
 }
