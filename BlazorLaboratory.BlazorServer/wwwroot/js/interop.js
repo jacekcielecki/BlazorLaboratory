383e1a0d2e00187a152899a6a4977ada06b258f3
Jacek Cielecki <jcielecki@sii.pl>
2023-12-07 14:23:12 +0100

    BlazorServer JsOperation

--- BlazorLaboratory.BlazorServer/wwwroot/js/interop.js
+++ BlazorLaboratory.BlazorServer/wwwroot/js/interop.js
@@ -5,3 +5,8 @@
 function showAlert() {
     alert("JsInterop test success!");
 }
+
+function showSnack() {
+    alert("showing snack...");
+    //componentRef.invokeMethodAsync("BlazorLaboratory.BlazorServer", "ShowSnackbar");
+}
